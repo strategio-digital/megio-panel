8800e556dd60fb6323ae8213b871c3d6c08a12ce
Jiří Zapletal <jz@strategio.dev>
2023-10-06 02:02:54 -0600

    v0.5.1

---
+++ www/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright (c) 2023 Strategio Digital s.r.o.
+ * @author Jiří Zapletal (https://strategio.dev, jz@strategio.dev)
+ */
+declare(strict_types=1);
+
+class index
+{
+    
+}
