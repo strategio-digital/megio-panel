03f9d3a855fe235d072786357b8ab2ea88718822
Jiří Zapletal <jz@strategio.dev>
2023-10-06 00:31:16 -0600

    Added manifest file

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    build: {
+        manifest: true,
+    },
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
-    }
+    },
+    plugins: [vue()]
 })
