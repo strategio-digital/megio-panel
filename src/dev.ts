16da4e62fa4e4fb9c19da566e873f62ea7322c92
Jiří Zapletal <jz@strategio.dev>
2024-03-14 10:43:48 -0600

    Fixed dev navbar

--- src/dev.ts
+++ src/dev.ts
@@ -1,17 +1,10 @@
 import { createMegioPanel, useGlobals } from './index'
-import { INavbarSettings } from './types'
 
 const globals = useGlobals()
 
-const brand: INavbarSettings['brand'] = {
-    title: 'Your brand name',
-    routeName: 'megio.view.dashboard',
-    logo: globals.navbar.brand
-}
-
 createMegioPanel('http://localhost:8090/', {
     navbar: {
-        brand,
+        brand: globals.navbar.brand,
         items: globals.navbar.items
     }
 })
